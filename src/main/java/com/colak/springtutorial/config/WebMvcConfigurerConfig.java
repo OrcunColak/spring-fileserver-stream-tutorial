7c14223260b4a1bd9ccbbc0124a696d32b9777fa
Orçun Çolak <orcun.colak@hazelcast.com>
2024-10-16 10:46:53 +0300

    async for servlet

---
+++ src/main/java/com/colak/springtutorial/config/WebMvcConfigurerConfig.java
@@ -0,0 +1,32 @@
+package com.colak.springtutorial.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// See https://howtodoinjava.com/spring-boot/spring-async-controller-responsebodyemitter/
+// Override the default async behavior
+@Configuration
+@EnableAsync
+public class WebMvcConfigurerConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+        configurer.setTaskExecutor(mvcTaskExecutor());
+        configurer.setDefaultTimeout(30_000);
+    }
+
+    @Bean
+    public ThreadPoolTaskExecutor mvcTaskExecutor() {
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setCorePoolSize(10);
+        threadPoolTaskExecutor.setMaxPoolSize(50);
+        threadPoolTaskExecutor.setQueueCapacity(100);
+        threadPoolTaskExecutor.setThreadNamePrefix("mvc-task-");
+
+        return threadPoolTaskExecutor;
+    }
+}
